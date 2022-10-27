06aee8332bad75ee3ac7ca404daae0e05f1dd1df
Choseongjin <sjcho@vantus.co.kr>
2022-10-27 09:10:02 +0900

    초기작업환경

---
+++ noticeAPI/src/main/java/com/notice/api/NoticeApiApplication.java
@@ -0,0 +1,13 @@
+package com.notice.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeApiApplication.class, args);
+	}
+
+}
---
+++ noticeAPI/src/test/java/com/notice/api/NoticeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notice.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
