1a7f32720c975ff14bb2eaf5f033bb0fda0bf23f
Choseongjin <sjcho@vantus.co.kr>
2022-10-27 10:08:51 +0900

    corsconfig 추가 , 프론트에서 통신할 테스트 컨트롤러 생성

--- noticeAPI/src/main/java/com/notice/api/user/controller/UserController.java
+++ noticeAPI/src/main/java/com/notice/api/user/controller/UserController.java
@@ -1,5 +1,37 @@
 package com.notice.api.user.controller;
 
-public class UserController {
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.notice.api.user.service.UserService;
+
+@RestController
+@RequestMapping(value="/notice-api")
+public class UserController {
+	
+private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	UserService userService;
+	
+	/**
+	 * 유저 주소목록 조회
+	 * @methodName : getUserAddr
+	 * @author     : choseongjin
+	 * @date       : 2022.06.30
+	 * @param userSrno
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/user/{username}/{password}", method=RequestMethod.GET)
+	public Map<String, Object> getUser(@PathVariable String username, @PathVariable String password) throws Exception {
+		return userService.getUser(username, password);
+	}
 }
--- noticeAPI/src/main/java/com/notice/api/user/service/UserService.java
+++ noticeAPI/src/main/java/com/notice/api/user/service/UserService.java
@@ -1,5 +1,23 @@
 package com.notice.api.user.service;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.notice.api.util.ResponseUtil;
+
+@Service
 public class UserService {
+	
+	@Autowired
+	ResponseUtil responseUtil;
+	
+	public Map<String, Object> getUser(String username, String password) throws Exception {
+		if ("1".equals(username) && "1".equals(password)) {
+			return responseUtil.resSuccess("로그인이 완료되었습니다.");
+		}
+		return responseUtil.resSuccess("로그인에 실패하였습니다.");
+	}
 
 }
