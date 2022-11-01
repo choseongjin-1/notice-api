536ac867e27c5ea55b698e70b5f9893ce683fb14
Choseongjin <sjcho@vantus.co.kr>
2022-11-01 11:29:19 +0900

    회원가입 컨트롤러, 서비스 구현

--- noticeAPI/src/main/java/com/notice/api/user/controller/UserController.java
+++ noticeAPI/src/main/java/com/notice/api/user/controller/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,22 +17,35 @@ import com.notice.api.user.service.UserService;
 @RequestMapping(value="/notice-api")
 public class UserController {
 	
-private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	UserService userService;
 	
 	/**
-	 * 유저 주소목록 조회
-	 * @methodName : getUserAddr
+	 * 유저 목록 조회
+	 * @methodName : getUser
 	 * @author     : choseongjin
-	 * @date       : 2022.06.30
-	 * @param userSrno
+	 * @date       : 2022.11.01
+	 * @param username
+	 * @param password
 	 * @return
 	 * @throws Exception
 	 */
 	@RequestMapping(value = "/user/{username}/{password}", method=RequestMethod.GET)
 	public Map<String, Object> getUser(@PathVariable String username, @PathVariable String password) throws Exception {
 		return userService.getUser(username, password);
+	}
+	
+	/**
+	 * 회원가입
+	 * @methodName : saveUser
+	 * @author     : choseongjin
+	 * @date       : 2022.11.01
+	 * @return
+	 */
+	@RequestMapping(value = "/user", method=RequestMethod.POST)
+	public Map<String, Object> saveUser(@RequestBody Map<String, Object> param) throws Exception {
+		return userService.saveUser(param);
 	}
 }
--- noticeAPI/src/main/java/com/notice/api/user/service/UserService.java
+++ noticeAPI/src/main/java/com/notice/api/user/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import com.notice.api.util.ResponseUtil;
 
@@ -13,11 +14,35 @@ public class UserService {
 	@Autowired
 	ResponseUtil responseUtil;
 	
+	/**
+	 * 유저 목록 조회
+	 * @methodName : getUser
+	 * @author     : choseongjin
+	 * @date       : 2022.11.01
+	 * @param username
+	 * @param password
+	 * @return
+	 * @throws Exception
+	 */
 	public Map<String, Object> getUser(String username, String password) throws Exception {
 		if ("1".equals(username) && "1".equals(password)) {
 			return responseUtil.resSuccess("로그인이 완료되었습니다.");
 		}
 		return responseUtil.resSuccess("로그인에 실패하였습니다.");
+	}
+	
+	/**
+	 * 회원가입
+	 * @methodName : saveUser
+	 * @author     : choseongjin
+	 * @date       : 2022.11.01
+	 * @return
+	 */
+	public Map<String, Object> saveUser(Map<String, Object> param) throws Exception {
+		if (!ObjectUtils.isEmpty(param)) {
+			return responseUtil.resSuccess("회원가입이 완료되었습니다.");
+		}
+		return responseUtil.resSuccess("회원가입에 실패하였습니다.");
 	}
 
 }
