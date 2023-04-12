52900d65c130ef6171ce2edd22fe7d62e0ffa1e0
Choseongjin <sjcho@vantus.co.kr>
2023-04-12 13:07:29 +0900

    war 배포를 위한 코드 추가

--- noticeAPI/src/main/java/com/notice/api/NoticeApiApplication.java
+++ noticeAPI/src/main/java/com/notice/api/NoticeApiApplication.java
@@ -2,9 +2,16 @@ package com.notice.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NoticeApiApplication {
+public class NoticeApiApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(NoticeApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoticeApiApplication.class, args);
